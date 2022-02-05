a636b7e253bc15d85b022c78a4e853f23969ef8c
Michael Ching <michaeljune101@yahoo.com>
2022-02-04 17:14:07 -0800

    FirstDrawing added

---
+++ Drawing.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Michael Ching
+ *
+ */
+public class Drawing
+{
+
+	
+	public void podiumDrawing() 
+	{
+		
+		System.out.println("     ________");
+		System.out.println("     |      |");
+		System.out.println("     |      ");
+		System.out.println("     |      ");
+		System.out.println("     |      ");
+		System.out.println("     |      ");
+		System.out.println("     |      ");
+		System.out.println("     |      ");
+		System.out.println(" ---------      ");
+
+	}
+}
--- Hangman.java
+++ Hangman.java
@@ -15,50 +15,25 @@ public class Hangman
 	public static void main(String[] args)
 	{
 		
-/*
+
 		System.out.println("Hello welcome to hangman!");
 		System.out.println("Enter a word to be guessed: ");
+		
 		Scanner scan = new Scanner(System.in);
-		
-		//String guessWord = scan.next();
-		
-		Console cnsl = System.console();
-		char[] ch = cnsl.readPassword(
-	            "Enter password : ");
-		String temp = new String (ch);
-		
-		System.out.println(temp);
-*/
-Scanner scan = new Scanner(System.in);
-		
 		String guessWord = scan.next();
 		
-		// Create the console object
-        Console cnsl
-            = System.console();
-  
-        if (cnsl == null) {
-            System.out.println(
-                "No console available");
-            return;
-        }
-  
-        // Read line
-        String str = cnsl.readLine(
-            "Enter username : ");
-  
-        // Print username
-        System.out.println(
-            "Username : " + str);
-  
-        // Read password
-        // into character array
-        char[] ch = cnsl.readPassword(
-            "Enter password : ");
-  
-        // Print password
-        System.out.println(
-            "Password : " + ch);
+		int chancesLeft = 7;
+		
+		while(chancesLeft >= 0)
+		{
+			
+			Drawing game = new Drawing();
+			game.podiumDrawing();
+			chancesLeft = -1;
+			
+		}
+		
+		
+		
 	}
-
 }
